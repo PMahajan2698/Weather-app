c6033f5ac69ead6bc5117bf9f0a15e9f76a02402
pmahajan2698 <pmahajan2698@gmail.com>
2019-08-07 14:12:14 +0530

    Changed Units

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/4e4bd29cda37ac53e8295564a9793917/' + latitude + ',' + longitude
+    const url = 'https://api.darksky.net/forecast/4e4bd29cda37ac53e8295564a9793917/' + latitude + ',' + longitude + '?units=auto'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. This high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress. This high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + '.')
         }
     })
 }
